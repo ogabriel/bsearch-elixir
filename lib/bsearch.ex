e16a0d2d542753636924fc203e63afcdd1e0e729
ogabriel <gabrieloliver8991@gmail.com>
2023-06-22 07:14:47 -0300

    add all base files

---
+++ lib/bsearch.ex
@@ -0,0 +1,18 @@
+defmodule Bsearch do
+  @moduledoc """
+  Documentation for `Bsearch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bsearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bsearch_test.exs
@@ -0,0 +1,8 @@
+defmodule BsearchTest do
+  use ExUnit.Case
+  doctest Bsearch
+
+  test "greets the world" do
+    assert Bsearch.hello() == :world
+  end
+end
