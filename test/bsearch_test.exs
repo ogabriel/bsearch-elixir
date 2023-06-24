7298eb7b7e7242b1cf1869f4bd1ee3905356d37b
ogabriel <gabrieloliver8991@gmail.com>
2023-06-24 11:38:23 -0300

    add tests

--- test/bsearch_test.exs
+++ test/bsearch_test.exs
@@ -2,6 +2,77 @@ defmodule BsearchTest do
   use ExUnit.Case, async: true
   doctest Bsearch
 
-  describe "member?/2" do
+  setup_all do
+    list = 1..100 |> Enum.to_list()
+    tuple = list |> List.to_tuple()
+
+    [integer_tuple: tuple, integer_list: list]
+  end
+
+  setup_all do
+    list = 1..100 |> Enum.map(fn _ -> Faker.Pokemon.name() end) |> Enum.sort() |> Enum.uniq()
+    tuple = list |> List.to_tuple()
+
+    [string_tuple: tuple, string_list: list]
+  end
+
+  describe "find_index/2" do
+    test "send list instead of tuple" do
+      assert_raise FunctionClauseError, fn ->
+        Bsearch.find_index([], 1)
+      end
+    end
+
+    test "send empty tuple" do
+      assert Bsearch.find_index({}, 1) == {:error, :not_found}
+    end
+  end
+
+  describe "find_index/2 with integer" do
+    test "send tuple with one value to find wrong value" do
+      assert Bsearch.find_index({2}, 1) == {:error, :not_found}
+    end
+
+    test "send tuple with one value to find correct value" do
+      assert Bsearch.find_index({1}, 1) == {:ok, 0}
+    end
+
+    test "send tuple with values to find smaller value", %{integer_tuple: tuple} do
+      assert Bsearch.find_index(tuple, -1) == {:error, :not_found}
+    end
+
+    test "send tuple with values to find bigger value", %{integer_tuple: tuple} do
+      assert Bsearch.find_index(tuple, 101) == {:error, :not_found}
+    end
+
+    test "send tuple with many values to find", %{integer_tuple: tuple, integer_list: list} do
+      Enum.with_index(list, fn value, index ->
+        assert Bsearch.find_index(tuple, value) == {:ok, index}
+      end)
+    end
+  end
+
+  describe "find_index/2 with string" do
+    test "send tuple with one value to find wrong value" do
+      assert Bsearch.find_index({"Eevee"}, "Flareon") == {:error, :not_found}
+    end
+
+    test "send tuple with one value to find correct value" do
+      assert Bsearch.find_index({"Vaporeon"}, "Vaporeon") == {:ok, 0}
+    end
+
+    test "send tuple with values to find smaller value", %{string_tuple: tuple} do
+      assert Bsearch.find_index(tuple, "Drago") == {:error, :not_found}
+    end
+
+    test "send tuple with values to find bigger value", %{string_tuple: tuple} do
+      assert Bsearch.find_index(tuple, "Agumon") == {:error, :not_found}
+    end
+
+    test "send tuple with many values to find", %{string_tuple: tuple, string_list: list} do
+      Enum.with_index(list, fn value, index ->
+        assert Bsearch.find_index(tuple, value) == {:ok, index}
+      end)
+    end
   end
 end
