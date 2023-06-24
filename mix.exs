04b0a06e1f36a06f460061c39d05c9383ee5c419
ogabriel <gabrieloliver8991@gmail.com>
2023-06-24 10:17:21 -0300

    add faker

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Bsearch.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:faker, "~> 0.17", only: :test}
     ]
   end
 end
