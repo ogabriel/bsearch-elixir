0302c90468f6824cea148e806ee9acd5461d5f7a
ogabriel <gabrieloliver8991@gmail.com>
2024-06-19 19:25:47 -0300

    update benchmark

--- benchmark/lib/benchmark.ex
+++ benchmark/lib/benchmark.ex
@@ -21,7 +21,7 @@ defmodule Benchmark do
     }
   end
 
-  def run do
+  def run_all() do
     run(10)
     run(100)
     run(1000)
@@ -35,10 +35,6 @@ defmodule Benchmark do
     map = list |> Enum.map(&{&1, true}) |> Map.new()
     inputs = generate_input(tuple)
 
-    run(inputs, tuple, list, map, index)
-  end
-
-  def run(inputs, tuple, list, map, index) do
     IO.puts("""
     #######################
     Starting Benchmark for #{index} items
@@ -55,7 +51,7 @@ defmodule Benchmark do
       warmup: 1,
       memory_time: 2,
       reduction_time: 2,
-      time: 10,
+      time: 5,
       formatters: [
         {Benchee.Formatters.Console, extended_statistics: true},
         {Benchee.Formatters.Markdown, file: "result_#{index}.md"}
