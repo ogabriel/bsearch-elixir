e691c1c66800a7e3d8d7d15bf6af3c4bbee0859b
ogabriel <gabrieloliver8991@gmail.com>
2024-06-20 18:51:50 -0300

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bsearch.MixProject do
   def project do
     [
       app: :bsearch,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
